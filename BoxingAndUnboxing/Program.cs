bda9ae58c86fdb5e0dd41b4cb27399c1d725be08
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-07 11:18:05 -0600

    Add project files.

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Kay");
+            list.Add(DateTime.Today);
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add();
+
+
+
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Boxing
+The process of converting a value tpye instance to an object reference
+
+int umber = 10;
+object obj = numberl
+
+//or
+objewct obj = 10;
+
+ 
+ */
